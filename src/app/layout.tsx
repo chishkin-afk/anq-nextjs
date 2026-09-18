1f373b2ffc60a274bc578fd8838f04cc0a80f3cf
chihskin-afk <hard.work.ath@gmail.com>
2026-09-18 09:49:39 +0300

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+interface RootLayoutProps {
+    children: ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
